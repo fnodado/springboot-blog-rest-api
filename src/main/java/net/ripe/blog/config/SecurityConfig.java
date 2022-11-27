12c2b810c7d32e185cf37b3cd373b9c48e3f9354
fnodado <francisdahryljava@gmail.com>
2022-11-27 11:11:55 +0800

    implementation of in memeory user details with bycrypt encoding password

--- src/main/java/net/ripe/blog/config/SecurityConfig.java
+++ src/main/java/net/ripe/blog/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -52,16 +53,17 @@ public class SecurityConfig {
      */
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+//        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
+
 
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails francis = User.builder().username("francis").password("password").roles("USER").build();
-        UserDetails admin = User.builder().username("admin").password("admin").roles("ADMIN").build();
+        UserDetails francis = User.builder().username("francis").password(passwordEncoder().encode("password")).roles("USER").build();
+        UserDetails admin = User.builder().username("admin").password(passwordEncoder().encode("admin")).roles("ADMIN").build();
 
         return new InMemoryUserDetailsManager(francis, admin);
     }
-
 
 }
