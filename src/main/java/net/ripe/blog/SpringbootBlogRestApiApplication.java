468ae34a01711cc10da67d029a52764086c2e6dc
fnodado <francisdahryljava@gmail.com>
2022-12-01 13:42:02 +0800

    implementation of multiple profiles

--- src/main/java/net/ripe/blog/SpringbootBlogRestApiApplication.java
+++ src/main/java/net/ripe/blog/SpringbootBlogRestApiApplication.java
@@ -1,13 +1,37 @@
 package net.ripe.blog;
 
+import net.ripe.blog.entity.Role;
+import net.ripe.blog.repository.RoleRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class SpringbootBlogRestApiApplication {
+public class SpringbootBlogRestApiApplication implements CommandLineRunner {
+
+	@Bean
+	public ModelMapper modelMapper() {return new ModelMapper();}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBlogRestApiApplication.class, args);
 	}
 
+
+	@Autowired
+	private RoleRepository roleRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Role adminRole = new Role();
+		adminRole.setName("ROLE_ADMIN");
+		roleRepository.save(adminRole);
+
+
+		Role userRole = new Role();
+		userRole.setName("ROLE_USER");
+		roleRepository.save(userRole);
+	}
 }
